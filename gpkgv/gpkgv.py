a0b061ce95b5a310eee04c24072d624dc6174974
Humbert Fiorino <humbert.fiorino@imag.fr>
2022-06-25 16:41:12 +0200

    Parse gpkgv arguments and options

--- gpkgv/gpkgv.py
+++ gpkgv/gpkgv.py
@@ -1,7 +1,12 @@
-import sys
+import argparse
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description = 'Geopackage validator')
+    parser.add_argument("geopackage", type = open, help = "geopackage to validate")
+    parser.add_argument("specification", type = open, help = "geopackage specification")
+    parser.add_argument("-r", "--report", type = argparse.FileType('w', encoding = 'utf-8'), help = "validation report")
+    parser.add_argument("-v", "--verbose", action = "store_true", help = "increase output verbosity")
+    parser.parse_args()
 
 if __name__ == '__main__':
     main()
